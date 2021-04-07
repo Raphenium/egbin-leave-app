19ca87c534a84781fb05e80e18c1d6411729577b
Raphenium <Gbengar36@gmail.com>
2021-04-07 09:23:53 +0100

    leave app

--- online-shop/shoppingbackend/src/main/java/net/kzn/shoppingbackend/dao/EpmRepObjAttrDAO.java
+++
@@ -1,9 +0,0 @@
-package net.kzn.shoppingbackend.dao;
-
-import java.util.List;
-
-import net.kzn.shoppingbackend.dto.EpmRepObjAttr;
-
-public interface EpmRepObjAttrDAO {
-	List<EpmRepObjAttr> list();
-}
--- online-shop/shoppingbackend/src/main/java/net/kzn/shoppingbackend/dao/StaffDetailsDAO.java
+++
@@ -1,9 +0,0 @@
-package net.kzn.shoppingbackend.dao;
-
-import java.util.List;
-
-import net.kzn.shoppingbackend.dto.StaffDetails;
-
-public interface StaffDetailsDAO {
-	List<Object[]> list();
-}
